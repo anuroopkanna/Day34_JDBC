7acaa0f76127d88d24a71fca385ef9c73e2fce66
anuroopkanna <anuroopindia16@gmail.com>
2023-04-21 03:56:55 +0530

    added:uc9

--- com/BridgeLabs/EmployeePayRollMain.java
+++ com/BridgeLabs/EmployeePayRollMain.java
@@ -29,7 +29,7 @@ public class EmployeePayRollMain {
 ////            System.out.println("Showing data present in the table");
 //            boolean val = stmt.execute("select * from emp_payroll  where name = 'Lohith'");
 //            System.out.println(val);
-            boolean val = stmt.execute("select * from employee_payroll");
+            boolean val = stmt.execute("select * from emp_payroll");
             if (val){
                 ResultSet rs = stmt.getResultSet();
                 while (rs.next()) {
@@ -42,27 +42,43 @@ public class EmployeePayRollMain {
                     System.out.print(", Phno: " + rs.getInt("phonenumber"));
                     System.out.print(", address: " + rs.   getString("address"));
                     System.out.print(", department: " + rs.   getString("department"));
+                    System.out.print(", basic_pay: " + rs.getInt("basic_pay"));
+                    System.out.print(", deductions: " + rs.getInt("deductions"));
+                    System.out.print(", taxable_pay: " + rs.getInt("taxable_pay"));
+                    System.out.print(", income_tax: " + rs.getInt("income_tax"));
+                    System.out.print(", net_pay: " + rs.getInt("net_pay"));
+
                     System.out.println();
                 }
             }else {
                 int count2=stmt.getUpdateCount();
                 System.out.println(count2);
             }
-            int isupdated = stmt.executeUpdate("alter table employee_payroll add phonenumber int after start");
-            int isupdated1 = stmt.executeUpdate("alter table employee_payroll add address varchar(100) after phonenumber");
-            int isupdated2 = stmt.executeUpdate("alter table employee_payroll add department varchar(50) after salary");
-            System.out.println(isupdated1);
-            System.out.println(isupdated2);
-            stmt.executeUpdate("update employee_payroll set phonenumber = 987648 where name = 'Lohith'");
-            stmt.executeUpdate("update employee_payroll set phonenumber = 649875 where name = 'Divya'");
-            stmt.executeUpdate("update employee_payroll set phonenumber = 654321 where name = 'Prathmesh'");
-            stmt.executeUpdate("update employee_payroll set phonenumber = 765778 where name = 'Swathi'");
-            stmt.executeUpdate("update employee_payroll set phonenumber = 675456 where name = 'Ravali'");
-            stmt.executeUpdate("update employee_payroll set address = 'Hyderbad' ,department='sales' where id = 1");
-            stmt.executeUpdate("update employee_payroll set address = 'Delhi' ,department='teamlead' where id = 2");
-            stmt.executeUpdate("update employee_payroll set address = 'Chennai' ,department='manager' where id = 3");
-            stmt.executeUpdate("update employee_payroll set address = 'Bengaluru' ,department='marketing' where id = 4");
-            stmt.executeUpdate("update employee_payroll set address = 'solapur' ,department='HR' where id = 5");
+//            int isupdated = stmt.executeUpdate("alter table emp_payroll add phonenumber int after start");
+//            int isupdated1 = stmt.executeUpdate("alter table emp_payroll add address varchar(100) after phonenumber");
+//            int isupdated2 = stmt.executeUpdate("alter table emp_payroll add department varchar(50) after salary");
+//            System.out.println(isupdated1);
+//            System.out.println(isupdated2);
+//            stmt.executeUpdate("update emp_payroll set phonenumber = 987648 where name = 'Lohith'");
+//            stmt.executeUpdate("update emp_payroll set phonenumber = 649875 where name = 'Divya'");
+//            stmt.executeUpdate("update emp_payroll set phonenumber = 654321 where name = 'Prathmesh'");
+//            stmt.executeUpdate("update emp_payroll set phonenumber = 765778 where name = 'Swathi'");
+//            stmt.executeUpdate("update emp_payroll set phonenumber = 675456 where name = 'Ravali'");
+//            stmt.executeUpdate("update emp_payroll set address = 'Hyderbad' ,department='sales' where id = 1");
+//            stmt.executeUpdate("update emp_payroll set address = 'Delhi' ,department='teamlead' where id = 2");
+//            stmt.executeUpdate("update emp_payroll set address = 'Chennai' ,department='manager' where id = 3");
+//            stmt.executeUpdate("update emp_payroll set address = 'Bengaluru' ,department='marketing' where id = 4");
+//            stmt.executeUpdate("update emp_payroll set address = 'solapur' ,department='HR' where id = 5");
+            int isupdated = stmt.executeUpdate("alter table emp_payroll add basic_pay int after salary");
+            int isupdated1 = stmt.executeUpdate("alter table emp_payroll add deductions int after basic_pay");
+            int isupdated2 = stmt.executeUpdate("alter table emp_payroll add taxable_pay int after deductions;");
+            int isupdated3 = stmt.executeUpdate("alter table emp_payroll add income_tax int after taxable_pay");
+            int isupdated4 = stmt.executeUpdate("alter table emp_payroll add net_pay int after income_tax");
+            stmt.executeUpdate(" update emp_payroll set basic_pay = 3000000.0 , deductions = 25000.0, taxable_pay = 300000.0, income_tax = 75000.0 , net_pay = 500000.0 where id = 1;");
+            stmt.executeUpdate(" update emp_payroll set basic_pay = 4000000.0 , deductions = 10000.0, taxable_pay = 600000.0, income_tax = 90000.0 , net_pay = 700000.0 where id = 2;");
+            stmt.executeUpdate("update emp_payroll set basic_pay = 3500000.0 , deductions = 20000.0, taxable_pay = 550000.0, income_tax = 80000.0 , net_pay = 600000.0 where id = 3;");
+            stmt.executeUpdate("update emp_payroll set basic_pay = 3000000.0 , deductions = 25000.0, taxable_pay = 250000.0, income_tax = 75000.0 , net_pay = 500000.0 where id = 4;");
+            stmt.executeUpdate("update emp_payroll set basic_pay = 2500000.0 , deductions = 30000.0, taxable_pay = 290000.0, income_tax = 70000.0 , net_pay = 400000.0 where id = 5;");
         } catch (Exception e){
             e.printStackTrace();
         }
