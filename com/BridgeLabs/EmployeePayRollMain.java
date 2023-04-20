8ee6f7b39fc63111f04659bc27329d1e04816fb8
anuroopkanna <anuroopindia16@gmail.com>
2023-04-21 02:47:32 +0530

    addedJDBCconnection

--- com/BridgeLabs/EmployeePayRollMain.java
+++ com/BridgeLabs/EmployeePayRollMain.java
@@ -3,9 +3,9 @@ package com.BridgeLabs;
 import java.sql.Connection;
 
 public class EmployeePayRollMain {
-    static  final String DB_URL = "jdbc:mysql://localhost:3306/payroll_servicee";
+    static  final String DB_URL = "jdbc:mysql://localhost:3306/payroll_servicees";
     static final String USER = "root";
-    static final String PASS = "root";
+    static final String PASS = "admin";
 
     public static void main(String[] args) {
         Connection connection;
